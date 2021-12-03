1193636011e6bfd235e16f4519630b8f3207bee6
Silas <silashoevers@gmail.com>
2021-12-03 19:51:45 +0100

    Finished Day 5 2020

--- 2020/Day_5.py
+++ 2020/Day_5.py
@@ -8,11 +8,21 @@ if __name__ == '__main__':
     for ticket in tickets:
         # Find row number
         possible_rows = range(128)
-        for cut in ticket[:8]:
+        for cut in ticket[:7]:
             if cut == 'F':
-                pass
+                possible_rows = possible_rows[:len(possible_rows)//2]
             elif cut == 'B':
-                pass
+                possible_rows = possible_rows[len(possible_rows)//2:]
 
         # Find column number
         possible_columns = range(8)
+        for cut in ticket[7:]:
+            if cut == 'L':
+                possible_columns = possible_columns[:len(possible_columns)//2]
+            elif cut == 'R':
+                possible_columns = possible_columns[len(possible_columns)//2:]
+            else:
+                print("Oops")
+
+        seat_ids.append(possible_rows[0] * 8 + possible_columns[0])
+    print("Part 1:", max(seat_ids))
