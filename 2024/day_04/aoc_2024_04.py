49708be0ae3a21fbfc87cce9564bfece42a84e0c
Silas <silashoevers@gmail.com>
2024-12-05 06:46:12 +0100

    Finished Y 2024 D 04

--- 2024/day_04/aoc_2024_04.py
+++ 2024/day_04/aoc_2024_04.py
@@ -1,6 +1,6 @@
 from aocd import get_data
 import numpy as np
-import re
+import regex as re
 
 def parse(puzzle_input):
     """Parse input."""
@@ -11,12 +11,11 @@ def count_xmas(lines):
     Counts how many times 'XMAS' occurs in the provided lines (list of lists of characters),
     both in order and in reverse order
     """
-    pattern = re.compile(r'XMAS')
+    pattern = re.compile(r'XMAS|SAMX')
     count = 0
     for line in lines:
         line = ''.join(line)
-        count += len(pattern.findall(line))
-        count += len(pattern.findall(line[::-1]))
+        count += len(pattern.findall(line, overlapped=True))
     return count
 
 def part1(data):
@@ -24,21 +23,51 @@ def part1(data):
     height, width = data.shape
     xmas_count = 0
 
-    # Go through all possible directions ("scanning lines") and per line check the reverse as well
+    # Go through all possible directions ("scanning lines")
     xmas_count += count_xmas([data[i, :] for i in range(height)])  # Horizontal
     xmas_count += count_xmas([data[:, j] for j in range(width)])   # Vertical
-    xmas_count += count_xmas([data.diagonal(offset) for offset in range(-height+1, height)])  # Diagonal, left to right
+    xmas_count += count_xmas([np.diag(data, k=offset) for offset in range(-height+1, height)])  # Diagonal, left to right
 
     # Diagonal, right to left
     data_fliplr = np.fliplr(data)  # Flip data horizontally
-    xmas_count += count_xmas([data_fliplr.diagonal(offset) for offset in range(-height+1, height)])
+    xmas_count += count_xmas([np.diag(data_fliplr, k=offset) for offset in range(-height+1, height)])
 
     return xmas_count
 
-
+def find_MAS_or_SAM(array):
+    """Per left-right diagonal in 2D array, return all matches of MAS or SAM as (offset, index_of_A)"""
+    height, width = array.shape
+    pattern = re.compile(r'MAS|SAM')
+    matches = []
+    for offset in range(-height+1, height):
+        matches_in_diag = pattern.finditer(''.join(np.diag(array, k=offset)), overlapped=True)
+        matches.extend([(offset, match_in_diag.span()[0]+1) for match_in_diag in matches_in_diag])
+    return matches
 
 def part2(data):
     """Solve part 2."""
+    height, width = data.shape
+
+    left_right_matches = find_MAS_or_SAM(data)
+    coordinates_left_right_matches = []
+    # Compute all (i,j) coordinates of As that are part of a MAS or SAM
+    for match in left_right_matches:
+        offset, index = match
+        # Big maths to revert diagonal coordinate back to regular coordinate
+        i = index + abs(min(0, offset))
+        j = index + max(0, offset)
+        coordinates_left_right_matches.append((i, j))
+
+    right_left_matches = find_MAS_or_SAM(np.fliplr(data))
+    coordinates_right_left_matches = []
+    for match in right_left_matches:
+        offset, index = match
+        i = index + abs(min(0, offset))
+        j = (width - 1) - (index + max(0, offset))
+        coordinates_right_left_matches.append((i, j))
+
+    return len(set(coordinates_left_right_matches) & set(coordinates_right_left_matches))
+
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
