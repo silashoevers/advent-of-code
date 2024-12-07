e7ee2a415787c43b338116960a1ef2a945ad4cce
Silas <silashoevers@gmail.com>
2024-12-07 21:39:08 +0100

    Wrote decently clean solution to Year 2024 Day 05

--- 2024/day_05/aoc_2024_05.py
+++ 2024/day_05/aoc_2024_05.py
@@ -1,36 +1,49 @@
+import pandas as pd
 from aocd import get_data
+from collections import Counter
+# Using compare function in sorted possible: https://docs.python.org/3/howto/sorting.html#comparison-functions
+from functools import cmp_to_key
+import numpy as np
+import numpy.ma as ma
 
 def parse(puzzle_input):
     """Parse input."""
     ordering_rules, updates = puzzle_input.split('\n\n')
     ordering_rules_parsed = [tuple(map(int, rule.split('|'))) for rule in ordering_rules.splitlines()]
-    updates_parsed = [tuple(map(int, update.split(','))) for update in updates.splitlines()]
+    updates_parsed = [list(map(int, update.split(','))) for update in updates.splitlines()]
     return ordering_rules_parsed, updates_parsed
 
-def is_update_correct(update, ordering_rules):
-    index_dict = {page_nr: index for index, page_nr in enumerate(update)}
-    update_correct = True
+def create_cmp_df(ordering_rules):
+    """Encode what ordering between page numbers we know:
+    -1: row < col, 0: row == col, 1: row > col, NaN: no comp known"""
+    possible_page_numbers = set([page for rule in ordering_rules for page in rule])
+    cmp_df = pd.DataFrame(index=list(possible_page_numbers), columns=list(possible_page_numbers))
     for before, after in ordering_rules:
-        if before in index_dict and after in index_dict:  # Are pages in ordering rule both present in our update?
-            if not index_dict[before] < index_dict[after]:
-                update_correct = False
-                break
-    return update_correct
+        cmp_df.loc[before,after], cmp_df.loc[after, before] = -1, 1
+    return cmp_df
 
 def part1(data):
     """Solve part 1."""
-    # Create entry to index dictionaries
     ordering_rules, updates = data
-    result = 0
+    cmp_df = create_cmp_df(ordering_rules)
+    middle_pages_sum = 0
     for update in updates:
-        update_correct = is_update_correct(update, ordering_rules)
-        if update_correct:
-            result += update[len(update) // 2]
-    return result
+        sorted_update = sorted(update, key=cmp_to_key(lambda a, b: cmp_df.loc[a,b]))
+        if update == sorted_update:  # Was update already correctly ordered?
+            middle_pages_sum += sorted_update[len(sorted_update) // 2]
+    return middle_pages_sum
 
 def part2(data):
     """Solve part 2."""
-    # Every number must appear as many times in update rules as there are other numbers
+    ordering_rules, updates = data
+    cmp_df = create_cmp_df(ordering_rules)
+    middle_pages_sum = 0
+    for update in updates:
+        sorted_update = sorted(update, key=cmp_to_key(lambda a, b: cmp_df.loc[a,b]))
+        if not update == sorted_update:  # Was update already correctly ordered?
+            middle_pages_sum += sorted_update[len(sorted_update) // 2]
+    return middle_pages_sum
+
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
